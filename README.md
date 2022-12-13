8e55cb089f02e3a0cf06ab706921090d69113ab7
ep-infosec <ep-infosec@users.noreply.github.com>
2022-12-13 19:44:08 +0200

    init

---
+++ README.md
@@ -0,0 +1,37 @@
+# mirror-branch action
+
+A Github Action to mirror one branch to another within the same repository
+
+For example, if you've renamed your `master` branch to `main`, but a lot of
+links out on the web hard-code the old name, you could mirror the `main` branch
+back to `master` so those links keep working.
+
+To use this Action, create a file called `.github/workflows/mirror.yml` with the
+following contents:
+
+```yaml
+on:
+  push:
+    branches:
+      - 'SOURCE_BRANCH_NAME'
+
+jobs:
+  mirror_job:
+    runs-on: ubuntu-latest
+    name: Mirror SOURCE_BRANCH_NAME branch to DESTINATION_BRANCH_NAME branch
+    steps:
+    - name: Mirror action step
+      id: mirror
+      uses: google/mirror-branch-action@v1.0
+      with:
+        github-token: ${{ secrets.GITHUB_TOKEN }}
+        source: 'SOURCE_BRANCH_NAME'
+        dest: 'DESTINATION_BRANCH_NAME'
+
+```
+
+With `SOURCE_BRANCH_NAME` and `DESTINATION_BRANCH_NAME` replaced as appropriate.
+
+-----
+
+This is not an officially supported Google product.
---
+++ index.ts
@@ -0,0 +1,67 @@
+/**
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+import type { OctokitResponse, GitUpdateRefResponseData } from '@octokit/types';
+
+async function run(): Promise<void> {
+  try {
+    const githubToken = core.getInput('github-token');
+    const sourceBranchName = core.getInput('source');
+    const destBranchName = core.getInput('dest');
+
+    const octokit = github.getOctokit(githubToken);
+    const sourceBranchTip = await octokit.git.getRef({
+      ...github.context.repo,
+      ref: `heads/${sourceBranchName}`,
+    });
+
+    if (sourceBranchTip.data.object.type !== "commit") {
+      throw new Error(`Expected branch ${sourceBranchName} to resolve to a commit. Got a ${sourceBranchTip.data.object.type}.`);
+    }
+
+    const sourceBranchSha = sourceBranchTip.data.object.sha;
+
+    console.log(`Pushing ${sourceBranchName} (${sourceBranchSha}) to ${destBranchName}.`);
+
+    let result: OctokitResponse<GitUpdateRefResponseData>;
+    try {
+      result = await octokit.git.updateRef({
+        ...github.context.repo,
+        ref: `heads/${destBranchName}`,
+        sha: sourceBranchSha,
+        force: true,
+      });
+    } catch (error) {
+      if (error.message !== 'Reference does not exist') {
+        throw error;
+      }
+      console.log(`${destBranchName} does not exist. Creating it.`);
+      result = await octokit.git.createRef({
+        ...github.context.repo,
+        ref: `refs/heads/${destBranchName}`,
+        sha: sourceBranchSha,
+      });
+    }
+
+    console.log(`Set ${result.data.ref} to ${result.data.object.sha}.`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
